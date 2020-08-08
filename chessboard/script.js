066c1f9af892185ff67e76f0e81c2ae96f7b9620
johnpiapian <johnpiapian@gmail.com>
2020-08-08 02:49:09 -0500

    Add animation to chess board

--- chessboard/script.js
+++ chessboard/script.js
@@ -1,7 +1,14 @@
 // Utility
-print = function(val){
+function print(val){
     console.log(val);
 }
+
+function addClass(el, _class){
+    if(el.hasAttribute('class')){
+        el.setAttribute('class', el.getAttribute('class') + _class);
+    }
+}
+// **********
 
 
 function generateLine(type, length){
@@ -76,6 +83,25 @@ function constructDOM(board){
 
 }
 
+function generateSquareIDs(){
+    let row = [1,2,3,4,5,6,7,8];
+    let column = ['a','b','c','d','e','f','g','h'];
+    let squares = [];
+
+    row.forEach(row => {
+        column.forEach(col => {
+            squares.push(row+col);
+        });
+    });
+
+    return squares;
+}
+
+function getSquare(cellID){
+    let el = document.querySelector("[data='"+cellID+"']");
+    return el;
+}
+
 function insertImg(element, url){
     let span = document.createElement('span');
         span.setAttribute('class', 'pawn')
@@ -89,18 +115,60 @@ function insertImg(element, url){
 }
 
 function boardClick_handler(e){
-    // return e.target;
     let element = e.target;
     let cellID = element.getAttribute('data');
 
-    insertImg(element, 'imgs/w_pawn.png');
+    // This will insert a pawn image to clicked square
+    // insertImg(element, 'imgs/w_pawn.png');
+
+    // print(element.id);
+    /* if(element.id === 'square'){
+        element.innerHTML = cellID;
+    } */
+
+    animation(generateSquareIDs());
+}
+
+function animation(squareIDs){
+    
+    let id = setInterval(frame, 5);
+    
+    let pos = 0;
+    let limit = 0;
+    let last = 0;
+
+    function frame() {
+
+        print(limit);
+        print(last);
+
+        if (pos >= last) {
+            if(squareIDs.length == limit){
+                clearInterval(id);
+            }else{
+                pos = 0;
+                last = squareIDs.length - limit;
+                limit++;
+            }
+        } else {
+            // getSquare(squareIDs[pos]).setAttribute('class',);
+
+            if(pos > 0){
+                getSquare(squareIDs[pos-1]).classList.remove('color-3');
+            }
+
+            getSquare(squareIDs[pos]).classList.add('color-3');
+
+            pos++;
+        }
+    }
+
 }
 
 function init(){
     var board = generateBoard(8);
         
     constructDOM(board);
-
 
     // Event handler
     document.getElementById('board').onclick = boardClick_handler;
